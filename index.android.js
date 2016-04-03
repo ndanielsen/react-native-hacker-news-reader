6d838f383f921fe7c658ea9dab8e24213ad2dd59
Nathan Danielsen <nathan.danielsen@gmail.com>
2016-04-03 17:41:57 -0400

    completed tutorial

--- index.android.js
+++ index.android.js
@@ -1,44 +1,51 @@
-'user strict';
-
+'use strict';
 var React = require('react-native');
 
-var { AppRegistry, StyleSheet, Navigator,} = React;
+var {
+  AppRegistry,
+  StyleSheet,
+  Navigator
+} = React;
+
 
 var NewsItems = require('./components/news-items');
 var WebPage = require('./components/webpage');
 
 var ROUTES = {
-    news_items: NewsItems,
-    web_page: WebPage,
+  news_items: NewsItems,
+  web_page: WebPage
 };
+
 
 var HnReader = React.createClass({
 
-    renderScene: function(route, navigator){
-        var Component = ROUTES[route.name];
+  renderScene: function(route, navigator) {
 
-        return (
-            <Component route={route} navigator={navigator} url={route.url}
-        );
-    },
+    var Component = ROUTES[route.name];
+    return (
+        <Component route={route} navigator={navigator} url={route.url} />
+    );
+  },
 
-    render: function(){
-        return (
-            <Navigator
-              style={styles.container}
-              initialRoute={{name:'news_items', url: ''}}
-              renderScene={this.renderScene}
-              configureScene={() => {return Navigator.SceneConfigs.FloatFromRight; }}
-              />
-        );
-    },
+  render: function() {
+    return (
+      <Navigator
+        style={styles.container}
+        initialRoute={{name: 'news_items', url: ''}}
+        renderScene={this.renderScene}
+        configureScene={() => { return Navigator.SceneConfigs.FloatFromRight; }} />
+    );
+
+  },
+
 
 });
 
+
 var styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    }
+  container: {
+    flex: 1
+  }
 });
 
 AppRegistry.registerComponent('HnReader', () => HnReader);
